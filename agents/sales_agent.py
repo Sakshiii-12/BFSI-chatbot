58ca4058d6130308ac2a25db25dfe6f69d6ce569
Sakshi <173380920+Sakshiii-12@users.noreply.github.com>
2025-12-06 14:41:14 +0530

    Delete agents directory

--- agents/sales_agent.py
+++
@@ -1,4 +0,0 @@
-class SalesAgent:
-    def propose_loan(self, customer):
-        limit = customer.get('pre_approved_limit', 0)
-        return f"Based on our analysis, your pre-approved personal loan limit is Rs.{limit:.0f}. Would you like to take this loan or request a different amount?"
